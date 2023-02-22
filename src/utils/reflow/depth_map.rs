5581d1055c4f8a845b28b928fdacaaa23c481e95
Ben King <9087625+benfdking@users.noreply.github.com>
2023-02-22 11:17:20 +0000

    Making progress on L001
    temp

--- src/utils/reflow/depth_map.rs
+++ src/utils/reflow/depth_map.rs
@@ -1,1 +1,20 @@
+/// An element of the stack_positions property of DepthInfo.
+#[derive(Debug, Clone)]
+pub struct StackPosition {
+    idx: usize,
+    len: usize,
+    position_type: String,
+}
+
+impl StackPosition {
+fn _stack_pos_interpreter(path_step: PathStep) -> String {
+
+}
+
+}
+
+#[derive(Debug, Clone)]
 pub struct DepthMap {}
+
+#[derive(Debug, Clone)]
+pub struct DepthInfo {}
